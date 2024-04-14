6156e512b98d993ec874bfa4ab2d9df3d80c89d9
member87 <43416057+member87@users.noreply.github.com>
2024-04-14 15:57:56 +0100

    added logger

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import { getImagesByCursor } from "@/app/actions";
+import { logger } from "@/app/utils/logger";
 import ImageContainer from "@/components/admin/imageContainer";
 
 
 export default async function Dashboard() {
 
   const res = await getImagesByCursor(0)
+  logger.info(res)
   return (
     <main className="h-screen p-4">
       <ImageContainer images={res.data} lastPage={res.page.lastPage == res.page.currentPage} />
